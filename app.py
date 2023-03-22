5c87cf9fb91389f18c7f491839b8d609168e48be
Isaiah-Lam <isaiah@lamison.net>
2023-03-22 12:34:12 -0400

    added minsweeper and difficulty for memory

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ class Leaderboards(database.Model) :
     userID = database.Column(database.Integer, database.ForeignKey("users.userID"))
     date = database.Column(database.Date)
     score = database.Column(database.Float)
+    difficulty = database.Column(database.String(25))
 
 # these lines add the models defined above to the database as tables
 
@@ -136,7 +137,7 @@ def memory():
 
 @app.route('/memory', methods=["POST"])
 def memoryScore():
-    submitScore(1, request.form["score"])
+    submitScore(1, request.form["score"], request.form["difficulty"])
     return redirect("/memory")
 
 
@@ -147,7 +148,6 @@ def rps():
 
 @app.route('/connect4', methods=["GET"])
 def connect4():
-    return render_template("connect4.html", loggedIn=(session.get("userid") is not None))
     highScore = 0
     if (session.get("userid") is None):
         highScore = "Login to see highscore"
@@ -156,19 +156,31 @@ def connect4():
         highScore = int(highScore.score)
     return render_template("rps.html", highScore=highScore, loggedIn=(session.get("userid") is not None))
 
+
 @app.route('/rps', methods={"POST"})
 def rpsScore():
     submitScore(2, request.form["score"])
     return redirect("/rps")
 
 
+@app.route('/minesweeper')
+def minsweeper():
+    return render_template("minesweeper.html", loggedIn=(session.get("userid") is not None))
+
+
+@app.route("/minesweeper", methods=["POST"])
+def minesweeperScore() :
+    submitScore(4, request.form["score"], request.form["difficulty"])
+    return redirect("/minesweeper")
+
+
 # function for submitting score to leaderboard
-def submitScore(gameID, score):
+def submitScore(gameID, score, difficulty=None):
     if (session.get("userid") is None):
         flash("You must be logged in to submit to the leaderboards")
     else:
         flash("Score submitted")
-        content = {"gameID":gameID, "userID":session.get("userid"), "date":date.today(), "score":score}
+        content = {"gameID":gameID, "userID":session.get("userid"), "date":date.today(), "score":score, "difficulty":difficulty}
         score = Leaderboards(**content)
         database.session.add(score)
         database.session.commit()
