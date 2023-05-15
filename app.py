1947bf7c46112c862ed0c636faeb0f72e8b7a3a5
Fasnacht <jhfasnacht211@stevenscollege.edu>
2023-05-15 14:44:19 -0400

    added games played for each user on account page

--- app.py
+++ app.py
@@ -147,9 +147,9 @@ def accountPage():
     if (session.get("userid") is None):
         return render_template("loginsignup.html", loggedIn=False)
     else:
-        user = Users.query.filter_by(userID=session.get("userid")).first()
-        print(user)
-        return render_template("account.html", userInfo=user, loggedIn=(session.get("userid") is not None))
+        user = con.execute(text(f'SELECT "username", "email" from users where "userID" = {session.get("userid")}')).first()
+        usergames = con.execute(text(f'SELECT games."gameID", games."title", max(leaderboards."score") FROM games JOIN leaderboards ON games."gameID" = leaderboards."gameID" WHERE leaderboards."userID" = {session.get("userid")} GROUP BY games."gameID", games."title"'))
+        return render_template("account.html", userInfo=user, usergames=usergames, loggedIn=(session.get("userid") is not None))
 
 
 @app.route('/login', methods=["GET"])
