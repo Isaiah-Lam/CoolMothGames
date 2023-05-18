ceb7036984c7d5870a19748c554980a4af32a2fb
Fasnacht <jhfasnacht211@stevenscollege.edu>
2023-05-18 13:44:09 -0400

    added some styling to accounts page

---
+++ static/account.js
@@ -0,0 +1,10 @@
+function openUsernameForm(){
+    $("#editbutton").css("display", "none");
+    $("#usernameform").css("display", "block");
+}
+
+function closeUsernameForm(){
+    $("#usernameform").css("display", "block");
+    $("#editbutton").css("display", "none");
+}
+
